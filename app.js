ff652b50ba67d96533f03293c4bd4ac17531231e
Axel-Ollivier <axel.ollivier56@outlook.com>
2021-11-19 14:47:30 +0100

    feat(all): ouais ça avance tavu

---
+++ app.js
@@ -0,0 +1,92 @@
+console.log("Hello World");
+
+async function getFilters() {
+  const fs = require("fs");
+  const dir = "./filters/";
+  validFilters = [];
+  files = fs.readdirSync(dir);
+  files.forEach((file) => {
+    filter = require(dir + file);
+    if (typeof filter === "function") {
+      validFilters.push(filter);
+    } else {
+      console.log("Invalid filter: " + file);
+    }
+  });
+  return validFilters;
+}
+
+async function getFilter(filter) {
+  return require("./filters/" + filter);
+}
+
+async function validateFilter(filter) {
+  if (typeof filter === "function") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function getConfigFilters() {
+  config = require("./config-filters.json");
+  return config;
+}
+
+async function validateConfigFilters(config) {
+  if (config.steps && Object.keys(config.steps).length > 0) {
+    Object.keys(config.steps).forEach((step) => {
+      if (config.steps[step].filter) {
+        if (validateFilter(getFilter(config.steps[step].filter))) {
+          if (
+            config.steps[config.steps[step].next] ||
+            !config.steps[step].next
+          ) {
+            return true;
+          } else {
+            console.log("Invalid next step: " + config.steps[step].next);
+            return false;
+          }
+        } else {
+          console.log("Invalid filter: " + config.steps[step].filter);
+          return false;
+        }
+      } else {
+        console.log("Missing filter on step " + step);
+        return false;
+      }
+    });
+  } else {
+    console.log("No steps found");
+    return false;
+  }
+}
+
+async function executeConfigFilters(config, step) {
+  getFilter(config.steps[step].filter).then((filter) => {
+    console.log(
+      "Step " + step + " - Executing filter: " + config.steps[step].filter
+    );
+    filter(config.steps[step].params).then((result) => {
+      console.log(config.steps[step]);
+      if (config.steps[config.steps[step].next]) {
+        console.log("Step " + step + " - Next step: " + config.steps[step].next);
+        executeConfigFilters(config, config.steps[step].next);
+      } else {
+        return;
+      }
+    });
+  });
+}
+
+getFilters().then((filters) => {
+  console.log(filters);
+  getConfigFilters().then((config) => {
+    if (validateConfigFilters(config)) {
+      console.log("Config is valid");
+      executeConfigFilters(config, "1");
+    } else {
+      console.log("Config is invalid");
+    }
+  });
+});
---
+++ filters/read.js
@@ -0,0 +1,14 @@
+module.exports = async function (params) {
+  const fs = require("fs").promises;
+
+  async function readFile(filePath) {
+    try {
+      const data = await fs.readFile(filePath);
+      console.log(data.toString());
+    } catch (error) {
+      console.error(`Got an error trying to read the file: ${error.message}`);
+    }
+  }
+
+  readFile(params[0]);
+};
