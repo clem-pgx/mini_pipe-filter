a771329b7dace920b470044c09d20c984736dbcc
clem-pgx <yeticlem79@hotmail.fr>
2021-11-19 16:02:57 +0100

    feat(all): write

--- filters/write.js
+++ filters/write.js
@@ -1,3 +1,8 @@
 module.exports = async function (str) {
+    fs = require('fs');
 
+    fs.writeFile(str[1], str[0], function (err) {
+        if (err) return console.log(err);
+        console.log(str[0] + ' > ' + str[1]);
+    });
 };
